00c2964b5f4c409375f4023caca805444d8c98fc
CH V N G Samba Siva Reddy <ssrchinta@gmail.com>
2022-04-14 11:34:16 +0530

    Link

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(express.json())
 // then get connection instance to know whether
 // connection is established or not
 
-const MONGODB_URL = process.env.MONGODB_URL
+const MONGODB_URL = "mongodb+srv://samba:samba123@cluster0.9i4ln.mongodb.net/myDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(MONGODB_URL, {
